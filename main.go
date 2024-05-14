89c55c80d47b5d9fd1c177afb25f7e125c6d0e9b
Dennis Kwong <denniskwong@auralabs.com.hk>
2024-05-14 11:05:41 +0800

    feat: add middlewares

--- main.go
+++ main.go
@@ -4,10 +4,17 @@ import (
 	"net/http"
 
 	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
 )
 
 func main() {
 	e := echo.New()
+	e.HideBanner = true
+
+	// Middleware
+	e.Use(middleware.Logger())
+	e.Use(middleware.Recover())
+	e.Use(middleware.Gzip())
 
 	e.GET("/", func(ctx echo.Context) error {
 		return ctx.String(http.StatusOK, "Hello")
